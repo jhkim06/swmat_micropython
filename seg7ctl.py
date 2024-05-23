891e3c632804325f54250969b75c10fe92f12544
jhkim <lukek06@gmail.com>
2024-05-23 17:24:27 +0900

    fix # of SPI block number

--- seg7ctl.py
+++ seg7ctl.py
@@ -1,8 +1,8 @@
 # example from https://www.instructables.com/Raspberry-Pi-Pico-MX7219-Eight-Digits-of-Seven-Seg/
 
-# clk -> GPIO16
-# DIN -> GPIO17
-# cs  -> GPIO19
+# clk -> GPIO10
+# DIN -> GPIO11
+# cs  -> GPIO13
 
 import time
 
@@ -29,11 +29,13 @@ def ex_upcount():
     return
 
 def display(line):
-    spi = SPI(0, baudrate=10000000, polarity=1, phase=0, sck=Pin(16), mosi=Pin(17))
-    ss = Pin(19, Pin.OUT)
+    # first parameter: # of SPI block of Pico
+    spi = SPI(1, baudrate=10000000, polarity=1, phase=0, sck=Pin(10), mosi=Pin(11))
+    ss = Pin(13, Pin.OUT)
 
     display = max7219_8digit.Display(spi, ss)
     display.write_to_buffer(line)
+    display.display()
 
     time.sleep(0.2)
 
